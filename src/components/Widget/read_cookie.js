4c362638945cab7e8d4c6f4d4b8ca32bf2b95c67
LHL670 <sally12066@gmail.com>
2021-08-20 15:26:40 +0800

    cookie init

---
+++ src/components/Widget/read_cookie.js
@@ -0,0 +1,6 @@
+function read_cookie(name) {
+    var result = document.cookie.match(new RegExp(name + '=([^;]+)'));
+    result && (result = JSON.parse(result[1]));
+    return result;
+   }
+export default read_cookie;
--- src/components/Widget/scholar.js
+++ src/components/Widget/scholar.js
@@ -11,8 +11,8 @@ class ScholarWidget extends React.Component {
 	constructor(props){ 
 		super(props);
 		this.state = {
-		message:'',
-		user:'',
+			message:'',
+			user:'',
 		};
 	}
 	static propTypes = {
@@ -23,6 +23,8 @@ class ScholarWidget extends React.Component {
 	};
 	componentDidMount() {
 		console.log(this.props);
+		const { cookies } = this.props;
+		
 		const id=this.props.id;
 
 		var self=this;
@@ -51,9 +53,13 @@ class ScholarWidget extends React.Component {
 			if(currentTime-timeStamp<2592000){//約1個月
 				var firebaseRef=db.collection('cguscholar').doc(`${id}`).get()
 				firebaseRef.then((dataSnapshot)=>{
-						self.setState({
+					var temp=dataSnapshot.data();
+					//cookies.set("scholar", JSON.stringify(temp), { path: "/" });
+					//console.log(JSON.parse(JSON.stringify(temp)));
+
+						/*self.setState({
 						message:dataSnapshot.data(),					
-					})		
+					})*/		
 				}).catch(error => {
 					console.log(error);
 							
@@ -64,16 +70,21 @@ class ScholarWidget extends React.Component {
 				
 			}
 		})
-		const { cookies } = this.props;
-		cookies.set("user", "gowtham", { path: "/" }); // setting the cookie
-		this.setState({ user: cookies.get("user") });			
+
+		
+		cookies.set("user", id, { path: "/" }); // setting the cookie
+		this.setState({ user: cookies.get("user") });	
+		
+		var cookieDataTemp=JSON.stringify(cookies.get("scholar"));
+		var cookieData=JSON.parse(cookieDataTemp);
+		this.setState({ message:cookieData });
+				
 	}
 	
 
 	
 	render() {
 		console.log(this.state.message);
-		console.log("user"+this.state.user);
 		if(this.props.size === 'medium'){
 			//m size
 		}
